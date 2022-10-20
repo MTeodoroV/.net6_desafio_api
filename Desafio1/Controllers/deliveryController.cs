93bbb62304cd75cd79d35069e8c8057ecbcbad17
Mateus <mateusteodorovieirabaptista@gmail.com>
2022-10-19 23:00:30 -0300

    Subindo o desafio 1

---
+++ Desafio1/Controllers/deliveryController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Desafio1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Desafio1.Controllers
+{
+    [ApiController]
+    [Route("/delivery")]
+    public class DeliveryController : ControllerBase
+    {
+        private static List<Delivery> Deliveries(){
+            return new List<Delivery>{
+                new Delivery{Id = 1, codeDelivery = "123", DateDelivery = new DateTime(2022, 10, 19)},
+                new Delivery{Id = 2, codeDelivery = "456", DateDelivery = new DateTime(2022, 09, 20)},
+                new Delivery{Id = 3, codeDelivery = "789", DateDelivery = new DateTime(2022, 05, 01)},
+            };
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Deliveries());
+        }
+
+        [HttpPost]
+        public IActionResult Post(Delivery delivery)
+        {
+            var deliveries = Deliveries();
+            deliveries.Add(delivery);
+            return Ok(deliveries);
+        }
+        
+    }
+}
---
+++ Desafio1/Models/delivery.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desafio1.Models
+{
+    public class Delivery
+    {
+        public int Id { get; set; }
+
+        public string codeDelivery { get; set; }
+
+        public DateTime DateDelivery { get; set; }
+    }
+}
